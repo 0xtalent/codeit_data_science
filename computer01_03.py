6d0df04d0b1f1e9dd04c772207b815e2f0ae9ac7
marinehayong <marinehayong@gmail.com>
2020-02-24 05:33:56 +0900

    오늘도 프로그래밍 공부 열심히! 인강 많이 듣기 꾸준히 많이 성실하게 지치지 말고

---
+++ computer01_03.py
@@ -0,0 +1,40 @@
+# 컴퓨터 개론 빠르게 끝내기
+# 200224 04:59
+# 추상화, retrun과 print의 차이
+# 복습 꼭 하기
+
+def print_square(x):
+    print(x * x)
+
+def get_square(x):
+    return x * x
+
+print(print_square(3))
+
+# return 문이 없으면, None이 return 됨
+# 즉시 종료하는 것도 알지?
+
+"""
+파라미터의 기본값을 설정해두면 함수 호출을 할 때 
+파라미터에 해당되는 값을 넘겨주지 않았을 경우, 그 파라미터는 기본값을 갖게 됩니다. 
+이런 파라미터를 optional parameter라고 부릅니다.
+
+Optional parameter는 모두 마지막에 있어야 합니다.
+
+x += 1
+x *= 2
+x -= 3
+x /= 2
+x %= 7
+
+위와 같이 자주 쓰이는 표현을 간략하게 쓸 수 있게 해 주는 문법을 syntactic sugar라고 합니다.
+
+https://www.codeit.kr/community/threads/10364
+"""
+
+def is_evenly_divisible(number):
+    return number % 2 == 0
+
+print(is_evenly_divisible(3))
+print(is_evenly_divisible(7))
+print(is_evenly_divisible(8))
