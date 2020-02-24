2a27478188ca6136cb9ed00cffc4dfcdc03cfe66
marinehayong <marinehayong@gmail.com>
2020-02-24 10:59:59 +0900

    프로그래밍 공부 열심히! 프로그래밍 언어 제대로 사용하기 끝

--- computer03_01.py
+++ computer03_01.py
@@ -1,3 +1,44 @@
-# 프로그래밍 언어 Overview
-# 200224 08:12
+# 프로그래밍 언어 제대로 사용하기
+# 200224 09:33
 
+"""
+math_score: snake 표기법
+MathScore: Pascal 표기법
+mathScore: camel 표기법
+"""
+
+"""
+독성 좋은 코드를 작성하는 데 있어서 굉장히 중요한 부분 중 하나가 “스타일 가이드”입니다. 
+(Convention이라고도 합니다.)
+
+Python 스타일 가이드
+
+Python의 경우 PEP8(Python Enhance Proposal 8)이라는 스타일 가이드가 있습니다.  
+Python 공식 스타일 가이드(일명: PEP8)  
+https://www.python.org/dev/peps/pep-0008/
+
+
+Google Python 스타일 가이드  
+글로벌 IT 기업인 Google에서 사용하는 스타일 가이드입니다. 
+PEP8과 비교해서 가장 다른 점은 “Docstring”을 작성하는 법에 대해 좀 더 깊이 다루고 있습니다.  
+https://github.com/google/styleguide/blob/gh-pages/pyguide.md
+
+JavaScript 스타일 가이드
+
+JavaScript 표준 스타일 가이드  
+https://standardjs.com/
+
+Airbnb JavaScript 스타일 가이드  
+https://github.com/airbnb/javascript
+
+Google JavaScript 스타일 가이드  
+https://google.github.io/styleguide/jsguide.html
+
+Java 스타일 가이드
+
+Java를 소유한 회사 오라클의 스타일 가이드  
+https://www.oracle.com/technetwork/java/codeconvtoc-136057.html
+
+Google Java 스타일 가이드  
+https://google.github.io/styleguide/javaguide.html
+"""
---
+++ computer03_02.py
@@ -0,0 +1,35 @@
+# 프로그래밍 언어 제대로 사용하기
+# 200224 10:45
+# 라이브러리 vs 프레임워크
+
+"""
+프레임워크는 큰 틀이 정해져 있고 세부 내용만 바뀌는 프로그램에 좋다!
+보안이나 암호화차람 까다롭고 어려운 부분들은 프레임워크가 알아서 해준다.
+
+훌륭한 개발자들의 코드를 잘 살펴보고, 그 코드는 어떻게 구조가 짜여져 있는지, 
+스타일 가이드는 어떻게 적용하고 있는지, 변수 이름이나 주석은 어떻게 쓰고 있는지, 
+어떤 라이브러리나 프레임워크를 사용하고 있는지, 이런 것들을 잘 살펴볼 필요가 있습니다.
+
+이 코드를 계속 관찰하고 마주칠 때마다 유심히 살펴보아야 좋은 코드를 눈에 익히고 
+내 것으로 만들어 실력을 늘릴 수 있습니다.
+
+좋은 코드를 찾을 수 있는 곳 1: 라이브러리
+
+코드를 공부할 만한 라이브러리를 몇 개 소개시켜드리겠습니다. 
+라이브러리 공식 홈페이지에는 라이브러리 사용법과 예시가 잘 나와있습니다.  
+Seaborn 라이브러리 - https://seaborn.pydata.org/tutorial.html  
+Pandas 라이브러리 - https://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html
+
+좋은 코드를 찾을 수 있는 곳 2: 개발 문서
+
+몇 가지 언어의 공식 사이트 링크를 공유드립니다.   
+Python - https://docs.python.org/ko/3/  
+Javascript - https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide  
+Ruby- https://www.ruby-lang.org/ko/documentation/  
+Java- https://docs.oracle.com/en/java/
+
+좋은 코드를 찾을 수 있는 곳 3: 커뮤니티
+
+구글 검색을 할 때 Best Practice라는 키워드로 검색을 해보면, 어떤 게 가장 좋은 코드인지 
+사람들의 논의 혹은 예시 코드들을 발견할 수 있을 겁니다.
+"""
